eab0bfe3cd34989d609f78da65b059b5f48cf83b
arslankhan8 <arslankhan52@gmail.com>
2021-01-09 20:18:01 -0500

    Part2 of previous commit

--- enclave/app/runperm.sh
+++ enclave/app/runperm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo '$0 = ' $0
 echo '$1 = ' $1
+echo '$2 = ' $2
 cat $1 > $PWD/temp
 make clean
 make app
-afl-fuzz  -m 50000000 -i ./in/ -o ./out/ ./app
+afl-fuzz  -m 50000000 -i $2/in/ -o $2/out/ ./app
